7f91d2e604300de203c21a9ed964bdd7ac83b30a
NiamhK07 <113034875+Niamh-Keenan@users.noreply.github.com>
2022-10-11 14:03:28 +0000

    Debugged

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-m=(input("Enter multiplication equation:"))
-num1=int(input("Enter the first integer:"))
-num2=int(input("Enter the second integer:"))
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+def multiply (x,y):
+
+ if y == 0:
+  return 0
+ else: 
+  count=y
+
+while count >0:
+  if x == 0:
+    count= 0
+    
+result +=x 
+count -= y
+print (result)
+
