42a2af89c9b43754fbca4d91739363ab560e6e1d
NiamhK07 <113034875+Niamh-Keenan@users.noreply.github.com>
2022-10-11 21:01:00 +0000

    completed program

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 x=int(input("Enter the first number:"))
 y=int(input("Enter the second number:"))
-def multiply (x,y):
+result = 0
 
- if y == 0:
-  return 0
- else: 
-  count=y
+if y < 0:   
+    count = -y 
+else:
+    count = y
 
-while count >0:
-  if x == 0:
-    count= 0
-    
-result +=x 
-count -= y
+while count > 0:
+    if x == 0: 
+        break   
+    result += x        
+    count -= 1       
 print (result)
 
